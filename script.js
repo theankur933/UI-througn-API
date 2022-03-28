928e879c1e8c45ca2e2d296e76d6b2961638246b
Ankur kharwar <ankurkharwar933@gmaill.com>
2022-03-29 00:43:02 +0530

    first commit

--- script.js
+++ script.js
@@ -9,23 +9,23 @@
 // }
 // abc.send();
 
-
+var array = ['Item 1', 'Item 2', 'Item 3'];
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index]);
+}
 fetch('https://business.paytm.com/api/v1/content/json?slug=blogs&live').then((response) => {
     return response.json();
 }).then((data) => {
-
-    // For 1st
-    document.getElementById('image').src = data[0].fields["blogImgUrl"];
-    document.getElementById('heading').innerHTML = data[0].title;
-    document.getElementById('link').innerHTML = data[0].actionCta[0].Name;
-
-    // For 2nd
-    document.getElementById('image1').src = data[1].fields["blogImgUrl"];
-    document.getElementById('heading1').innerHTML = data[1].title;
-    document.getElementById('link1').innerHTML = data[1].actionCta[0].Name;
-
-    // For 3rd
-    document.getElementById('image2').src = data[2].fields["blogImgUrl"];
-    document.getElementById('heading2').innerHTML = data[2].title;
-    document.getElementById('link2').innerHTML = data[2].actionCta[0].Name;
+    var result = '';
+    var abc = document.getElementById('main')
+    console.log(data);
+    for (let index = 0; index < data.length; index++) {
+        result += '<div class="container">';
+        result += '<img src=' + data[index].fields.blogImgUrl + ' />';
+        result += '<h2 id="heading">' + data[index].title + '</h2>';
+        result += '<a href="' + data[index].fields.actionLinkUrl + '" id="link">' + data[index].fields.actionLinkName + '</a>';
+        result += '</div >'
+    }
+    abc.innerHTML = result;
+    console.log(typeof data)
 })
